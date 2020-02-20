5aa29a6f9768f3df51a403af3adbd7391d262a62
Eryk Kowalewicz <eryk.kowalewicz@outlook.com>
2020-02-20 14:09:41 +0000

    Modified Files

--- Date.java
+++ Date.java
@@ -4,30 +4,39 @@
 public class Date {
     
     // Instance Variables
-    private int day;
-    private int month;
-    private int year;
+    int day;
+    int month;
+    int year;
 
     // Default Constructor (No Parameters)
     public Date(){
-        //Your code here
+        day = 1;
+        month = 1;
+        year = 1;
     }
 
     // Parameterized constructor (3 Parameters)
     public Date(int newDay, int newMonth, int newYear){
-        // Your code here.
+        day = newDay;
+        month = newMonth;
+        year = newYear;
     }
     
     // Parameterized constructor (2 Parameters)
-    // Your code here.
+    public Date(int newDay, int newMonth){
+        day = newDay;
+        month = newMonth;
+    }
     
     // Parameterized constructor (1 Parameter)
-    // Your code here.
+    public Date(int newDay){
+        day = newDay;
+    }
     
     // A simple print method
     public void printDate(){
         // Print the date like this: day/month/year
-        // Your code here
+        System.out.println(day+"/"+month+"/"+"/"+year);
     }
 
 }
--- Main.java
+++ Main.java
@@ -4,7 +4,15 @@
 public class Main {
     public static void main(String[] args) {
         // Create a Date object using the default constructor
+        Date dateObject1 = new Date();
+        System.out.println(dateObject1.year);
         // Create 3 more Date objects using all the paramiterised constructors
+        Date dateObject2 = new Date(20, 1, 2020);
+        
+        Date dateObject3 = new Date(31);
+        System.out.println(dateObject3.day);
+        System.out.println(dateObject3.year);
         // Call the printDate() method for each object you create.
+        //printDate();
     }
 }
